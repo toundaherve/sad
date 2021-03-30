54a450362d0b88fe5e0a1ed6961366f92b4f433a
toundaherve <toundaherve@gmail.com>
2021-03-30 20:56:25 +0100

    Add Signup Logic

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,112 +1,44 @@
-import React, { MouseEvent, ReactNode, useState } from "react"
+import React, { MouseEvent, ReactNode, useEffect, useRef, useState } from "react"
+import { useForm } from "react-hook-form"
 import { Button, Input } from "reactstrap"
 
-const STEP_ACCOUNT_CREATION = "ACCOUNT_CREATION"
-const STEP_EMAIL_VERIFICATION = "EMAIL_VERIFICATION "
-const STEP_PASSWORD_CREATION = "PASSWORD_CREATION"
-
-type Step = typeof STEP_ACCOUNT_CREATION | typeof STEP_EMAIL_VERIFICATION | typeof STEP_PASSWORD_CREATION
-
 type Account = {
-    name: string,
-    email: string,
-    password: string
+    name: string;
+    email: string;
+    country: string;
+    city: string;
 }
 
+type Step = "account_creation" | "email_verification" | "password_creation"
+
 function SignupPage() {
-    const [loading, setLoading] = useState(false)
-    const [currentStep, setCurrentStep] = useState<Step>(STEP_ACCOUNT_CREATION)
-    const [account, setAccount] = useState<Account>({name: "", email: "", password: ""})
-    const [emailAvailable, setEmailAvailable] = useState(false)
-    const [verificationCode, setVerificationCode] = useState("")
-    const [nextDisabled, setNextDisabled] = useState(true)
+    const [visibleNextButton, setVisibleNextButton] = useState(false)
+    const [visiblePreviousButton, setVisiblePreviousButton] = useState(false)
+    const [disabledNextButton, setDisabledNextButton] = useState(true)
+    const [disabledPreviousButton, setDisabledPreviousButton] = useState(false)
+    const [currentStep, setCurrentStep] = useState<Step>("account_creation")
+    const [account, setAccount] = useState<Account>({name: "", email: "", country: "", city: ""})
+    const nextButtonRef = useRef(null) 
 
-    function renderCurrentStepForm() {
-        switch (currentStep) {
-            case STEP_EMAIL_VERIFICATION:
-                return <EmailVerificationForm email={account.email} code={verificationCode} onChange={handleChange} />
-            case STEP_PASSWORD_CREATION:
-                return <PasswordCreationForm password={account.password} onChange={handleChange} />
-            default:
-                return <AccountCreationForm account={account} onChange={handleChange} onBlur={checkEmailAvailability} onSubmit={next} emailAvailable={emailAvailable} />
+    function handleSuccessAccountCreation(account : Account) {
+        setAccount(account)
+        setCurrentStep("email_verification")
+        setVisibleNextButton(true)
+        setVisiblePreviousButton(true)
+    }
+
+    function handleSuccessEmailVerification() {
+        setCurrentStep("password_creation")
+        setDisabledNextButton(true)
+        setVisiblePreviousButton(false)
+    }
+
+    function handlePreviousButtonClick() {
+        if (currentStep === "email_verification") {
+            setCurrentStep("account_creation")
+            setVisiblePreviousButton(false)
+            setVisibleNextButton(false)
         }
-    }
-
-    function handleChange(e: React.FormEvent<HTMLInputElement>) {
-        const {name, value} = e.currentTarget
-        switch (name) {
-            case "name":
-                setAccount({...account, name: value})
-                break;
-            case "email":
-                setAccount({...account, email: value})
-                break;
-            case "password":
-                setAccount({...account, password: value})
-                if (!account.password) {
-                    setNextDisabled(true)
-                } else {
-                    setNextDisabled(false)
-                }
-                break;
-            case "code":
-                setVerificationCode(value)
-                if (!verificationCode) {
-                    setNextDisabled(true)
-                } else {
-                    setNextDisabled(false)
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    function next(e : MouseEvent<HTMLButtonElement>) {
-        switch (currentStep) {
-            case STEP_ACCOUNT_CREATION:
-                setCurrentStep(STEP_EMAIL_VERIFICATION)
-                break;
-            case STEP_EMAIL_VERIFICATION:
-                verifyEmail()
-                setNextDisabled(true)
-                break
-            case STEP_PASSWORD_CREATION:
-                finilizeAccountCreation()
-                break
-            default:
-                break;
-        }        
-
-    }
-
-    function previous(e : MouseEvent<HTMLButtonElement>) {
-        switch (currentStep) {
-            case STEP_ACCOUNT_CREATION:
-                break;
-            case STEP_EMAIL_VERIFICATION:
-                setCurrentStep(STEP_ACCOUNT_CREATION)
-                break
-            case STEP_PASSWORD_CREATION:
-                break
-            default:
-                break;
-        }        
-
-    }
-
-    function checkEmailAvailability(e: React.FormEvent<HTMLInputElement>) {
-        alert('Checking email availability')
-        setEmailAvailable(true)
-    }
-    
-    function verifyEmail(){
-        alert('Verifiying email')
-        setCurrentStep(STEP_PASSWORD_CREATION)
-    }
-
-    function finilizeAccountCreation() {
-        alert('Finilizing account creation')
     }
 
     return (
@@ -117,60 +49,159 @@ function SignupPage() {
                         <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white px-3">
                             <div className="d-flex align-items-center justify-content-center" style={{minHeight: "53px"}}>
                                 <span className="d-flex flex-column justify-content-center flex-grow-1 flex-shrink-1 align-items-start align-self-stretch" style={{minWidth: "56px", minHeight: "32px", flexBasis: "50%"}}>
-                                    {currentStep === STEP_EMAIL_VERIFICATION && <Button onClick={previous}>Previous</Button>}
+                                    {visiblePreviousButton && <Button disabled={disabledPreviousButton} onClick={handlePreviousButtonClick}>Previous</Button>}
                                 </span>
                                 <span>Logo</span>
                                 <span className="d-flex flex-column justify-content-center flex-grow-1 flex-shrink-1 align-items-end align-self-stretch" style={{minWidth: "56px", minHeight: "32px", flexBasis: "50%"}}>
-                                    {currentStep !== STEP_ACCOUNT_CREATION && <Button onClick={next} disabled={nextDisabled}>Next</Button>}
+                                    {visibleNextButton && <Button disabled={disabledNextButton} innerRef={nextButtonRef}>Next</Button>}
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
                 
-                {renderCurrentStepForm()}
+               {currentStep === "account_creation" && <AccountCreationForm onSuccessAccountCreation={handleSuccessAccountCreation} />}
+               {currentStep === "email_verification" && <EmailVerificationForm email={account.email} onValidCode={() => setDisabledNextButton(false)} onInvalidCode={() => setDisabledNextButton(true)} submitButtonRef={nextButtonRef} onSuccess={handleSuccessEmailVerification} />}
+               {currentStep === "password_creation" && <PasswordCreationForm onValidPassword={() => setDisabledNextButton(false)} onInvalidPassword={() => setDisabledNextButton(true)} submitButtonRef={nextButtonRef} onSuccess={() => { alert("Done!!") }} />}
             </div>
         </div>
     )
 }
 
 type AccountCreationFormProps = {
-    account: Account
-    onBlur: React.FormEventHandler<HTMLInputElement>
-    onChange: React.FormEventHandler<HTMLInputElement>
-    onSubmit: React.MouseEventHandler<HTMLButtonElement>
-    emailAvailable: boolean
+    onSuccessAccountCreation: (data: Account) => void
 }
 
-function AccountCreationForm({account, onBlur, onChange, onSubmit,emailAvailable}: AccountCreationFormProps) {
+function AccountCreationForm({onSuccessAccountCreation}: AccountCreationFormProps) {
+    const {register, errors, getValues,trigger} = useForm<Account>()
+    const [checkingEmailTakenDone, setCheckingEmailTakenDone] = useState(false)
+    const [emailTaken, setEmailTaken] = useState(false)
+    const [buttonDisactivated, setButtonDisactivated] = useState(true)
+
+    let typingTimer: NodeJS.Timeout
+    let doneTypingInterval = 1000
+    
+    function handleKeyUp( e: React.FormEvent<HTMLInputElement>) {
+        const name = e.currentTarget.name as keyof Account
+        clearTimeout(typingTimer)
+        typingTimer = setTimeout( async() => {
+            const validEmail = await trigger(name)
+            if (validEmail) {
+                console.log("Verifying email available")
+                setTimeout(() => {
+                    setCheckingEmailTakenDone(true)
+                    setEmailTaken(false)
+                    activateButton()
+                }, 1000)
+                activateButton()
+            }
+        }, doneTypingInterval)
+        activateButton()
+    }
+
+    function handleKeyDown(e: React.FormEvent<HTMLInputElement>){
+        clearTimeout(typingTimer)
+    }
+
+    function handleChange(e : React.FormEvent<HTMLInputElement>) {
+        const name = e.currentTarget.name as keyof Account
+        trigger(name).then(() => activateButton())
+    }
+
+    function handleBlur(e: React.FormEvent<HTMLInputElement>) {
+        const name = e.currentTarget.name as keyof Account
+        trigger(name).then(() => activateButton())
+    }
+
+    function activateButton() {
+        const values = getValues()
+       const allFieldsFilled = Object.keys(values).every((key)  => values[key as keyof Account] !== "")
+       const noErrors = Object.keys(errors).length === 0
+
+       if (allFieldsFilled && noErrors && checkingEmailTakenDone && !emailTaken) {
+           setButtonDisactivated(false)
+       } else {
+           setButtonDisactivated(true)
+       }
+    }
+
+    function handleSubmit() {
+        console.log("Loading !!!")
+        console.log("Sending request")
+        setTimeout(() => {
+            console.log("Response back")
+            onSuccessAccountCreation(getValues())
+            console.log("Stopped loading")
+        }, 3000)
+    }
+
     return (
         <FormTemplate title="Create your Account">
             <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
-                <Input placeholder="Name" type="text" name="name" value={account.name} onChange={onChange} />
+                <Input placeholder="Name" type="text" name="name" innerRef={register({required: true, maxLength: 50})} onChange={handleChange}  onBlur={handleBlur} />
+                {errors.name && <small className="text-danger">Please provide a name.</small>}
             </div>
             <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
-                <Input placeholder="Email" type="email" name="email" value={account.email} onChange={onChange} onBlur={onBlur} />
+                <Input placeholder="Email" type="email" name="email" innerRef={register({required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/})} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} onBlur={handleBlur}   />
+                {errors.email && <small className="text-danger">Please provide a valid email.</small>}
+                {checkingEmailTakenDone && emailTaken && <small className="text-danger">Email already taken.</small> }
+            </div>
+            <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
+                <Input placeholder="Country" type="text" name="country" innerRef={register({required: true})} onChange={handleChange} onBlur={handleBlur}   />
+                {errors.country && <small className="text-danger">Please provide your country.</small>}
+            </div>
+            <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
+                <Input placeholder="City" type="text" name="city" innerRef={register({required: true})} onChange={handleChange} onBlur={handleBlur}   />
+                {errors.city && <small className="text-danger">Please provide your city.</small>}
             </div>
             <p className="mb-0 p-0" style={{marginTop: "64px"}}>
                 By signing up, you agree to our <a href="#1">Terms</a>, <a href="#1">Privicy Policy</a>, and <a href="#1">Cookie Use</a>.
             </p>
-            <Button className="mt-3" disabled={!emailAvailable} onClick={onSubmit}>Sign up</Button>
+            <Button className="mt-3" onClick={handleSubmit} disabled={buttonDisactivated} type="button">Sign up</Button>
         </FormTemplate>
     )
 }
 
 type EmailVerificationFormProps = {
-    email: string,
-    code: string,
-    onChange: React.FormEventHandler<HTMLInputElement>
+    email: string;
+    onValidCode: () => void;
+    onInvalidCode: () => void;
+    submitButtonRef: React.MutableRefObject<HTMLButtonElement | null>;
+    onSuccess: () => void
 }
 
-function EmailVerificationForm({email, code, onChange}: EmailVerificationFormProps) {
+function EmailVerificationForm({email, onValidCode, onInvalidCode, submitButtonRef, onSuccess}: EmailVerificationFormProps) {
+    const { watch, register } = useForm<{code : string}>()
+    const code = watch("code")
+    
+    useEffect(() => {
+        if (code) {
+            onValidCode()
+        } else {
+            onInvalidCode()
+        }
+
+    }, [onValidCode, onInvalidCode, code])
+
+    if (submitButtonRef !== null && submitButtonRef.current !== null && submitButtonRef.current.onclick !== null) {
+        submitButtonRef.current.onclick = handleSubmit
+    }
+
+    function handleSubmit(e : globalThis.MouseEvent) {
+        console.log("Loading...")
+        console.log("sending request with code ", code)
+        setTimeout(() => {
+            console.log("response back")
+            onSuccess()
+            console.log("Stopped Loading...")
+        }, 3000)
+    }
+
     return (
         <FormTemplate title="We sent you a code">
-            <p className="m-0 p-0" >Enter it below to verify {email}.</p>
+            <p className="m-0 p-0" >Enter it below to verify {email}</p>
             <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
-                <Input placeholder="Verification code" type="number" name="code" value={code} onChange={onChange} />
+                <Input placeholder="Verification code" type="number" name="code" innerRef={register({required: true})} />
                 <small>Didn't receive email?</small>
             </div>
         </FormTemplate>
@@ -178,16 +209,46 @@ function EmailVerificationForm({email, code, onChange}: EmailVerificationFormPro
 }
 
 type PasswordCreationFormProps = {
-    password: string,
-    onChange: React.FormEventHandler<HTMLInputElement>
+    onValidPassword: () => void;
+    onInvalidPassword: () => void;
+    submitButtonRef: React.MutableRefObject<HTMLButtonElement | null>;
+    onSuccess: () => void
 }
 
-function PasswordCreationForm({password, onChange}: PasswordCreationFormProps) {
+function PasswordCreationForm({onValidPassword, onInvalidPassword, submitButtonRef, onSuccess} : PasswordCreationFormProps) {
+    const {register, watch, trigger} = useForm<{password: string}>()
+    const password = watch("password")
+
+    useEffect(() => {
+        trigger("password")
+        .then(valid => {
+            if(valid) {
+                onValidPassword()
+            } else {
+                onInvalidPassword()
+            }
+        })
+    }, [trigger, onInvalidPassword, onValidPassword, password])
+
+    if (submitButtonRef !== null && submitButtonRef.current !== null && submitButtonRef.current.onclick !== null) {
+        submitButtonRef.current.onclick = handleSubmit
+    }
+
+    function handleSubmit(e : globalThis.MouseEvent) {
+        console.log("Loading...")
+        console.log("sending request with password ", password)
+        setTimeout(() => {
+            console.log("response back")
+            onSuccess()
+            console.log("Stopped Loading...")
+        }, 3000)
+    }
+
     return (
         <FormTemplate title="You'll need a password">
             <p className="m-0 p-0" >Make sure it's 8 characters or more.</p>
             <div className="" style={{paddingTop: "12px", paddingBottom: "12px"}}>
-                <Input placeholder="Password" type="password" name="password" value={password} onChange={onChange} />
+                <Input placeholder="Password" type="password" name="password" innerRef={register({required: true, minLength: 8})} />
                 <small>Reveal password</small>
             </div>
         </FormTemplate>
